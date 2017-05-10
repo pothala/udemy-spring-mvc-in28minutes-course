af07c4d34a9dd6f882e54d8f840008138bd85291
Navid Rojiani <nrojiani@gmail.com>
2017-05-09 21:54:56 -0700

    end of video 9 (dispatching & JSP)

---
+++ first-webapp/src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,17 @@
+package webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp")     /* starts from WEB-INF */
+                .forward(request, response);
+    }
+}
---
+++ first-webapp/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nrojiani
+  Date: 5/9/17
+  Time: 9:40 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Navid's Web App</title>
+</head>
+<body>
+    <p>Hello, from JSP!</p>
+</body>
+</html>
