e2d6bb623a26d632228483fddfe885f16f87968f
Navid Rojiani <nrojiani@gmail.com>
2017-06-08 22:07:37 -0700

    End of Step 17: Login Authentication

--- spring-mvc-webapp/src/main/java/com/in28minutes/springmvc/LoginController.java
+++ spring-mvc-webapp/src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.springmvc;
 
+import com.in28minutes.login.LoginService;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Log4j
 @Controller
 public class LoginController {
+
+    @Autowired
+    public LoginService loginService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage() {
@@ -21,6 +26,12 @@ public class LoginController {
     public String handleUserLogin(ModelMap model,
                                   @RequestParam String name,
                                   @RequestParam String password) {
+
+        if (!loginService.isValidUser(name, password)) {
+            model.put("errorMessage", "Invalid Credentials");
+            return "login";
+        }
+
         model.put("name", name);
         model.put("password", password);
         return "welcome";
